030e15bb4486c94d5b4b8f8d247afea71c324c25
Yuen Ler Chow <lerchow@gmail.com>
2024-02-17 13:34:40 -0800

    Fix form validation and add alert message

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -4,16 +4,22 @@ const OnboardingPage = ({ onOnboardingComplete }) => {
   const [name, setName] = useState('');
   const [grade, setGrade] = useState('');
   const [id, setId] = useState('');
+  const [showAlert, setShowAlert] = useState(false); // State to manage alert visibility
 
   const handleFinish = () => {
-    if (name && grade && id) {
+    if (!name && grade && id) {
       onOnboardingComplete(name, grade, id);
+      setShowAlert(false); // Hide alert when all fields are filled
+    } else {
+      setShowAlert(true); // Show alert when any field is empty
     }
   };
+
 
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="w-full max-w-md p-4 bg-white shadow-md rounded-lg">
+
         <div>
           <label htmlFor="name" className="block text-lg font-medium text-gray-700 mb-2">Name</label>
           <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm mb-2" />
@@ -29,10 +35,20 @@ const OnboardingPage = ({ onOnboardingComplete }) => {
           <label htmlFor="id" className="block text-lg font-medium text-gray-700 mb-2">ID Number</label>
           <input type="text" id="id" value={id} onChange={(e) => setId(e.target.value)} className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm mb-2" />
         </div>
-        <button onClick={handleFinish} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 w-full">
+        <button onClick={handleFinish}
+          className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
+        >
           Finish
         </button>
       </div>
+      {showAlert && (
+        <div className="text-white px-6 py-2 border-0 rounded relative mb-4 bg-red-500">
+          <span className="inline-block align-middle mr-8">
+            Please fill out all fields before finishing.
+          </span>
+        </div>
+      )}
+
     </div>
   );
 };
